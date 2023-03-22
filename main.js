58287880072acce12c3fcfbad0aac7946d0e514b
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-03-22 16:17:35 +0100

    Done and dusted

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector("#menu-btn");
 let header = document.querySelector(".header");
-
+let more = document.getElementById("more");
+let showmore = document.getElementById("showmore");
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   header.classList.toggle("active");
@@ -22,12 +23,19 @@ themeToggler.onclick = () => {
   }
 };
 const progress = document.querySelectorAll(".progress h3 span");
-const progressBar = document.querySelectorAll(".progress div span" );
+const progressBar = document.querySelectorAll(".progress div span");
 let i = 0;
-for(let span of progress){
-	progressBar[i++].style.width =`${span.textContent}`
-	
-	
-console.log(span.textContent);
-}
+for (let span of progress) {
+  progressBar[i++].style.width = `${span.textContent}`;
 
+  console.log(span.textContent);
+}
+showmore.addEventListener("click", () => {
+  if (more.style.display == "none") {
+    more.style.display = "block";
+    showmore.innerText = "Show less";
+  } else {
+    more.style.display = "none";
+    showmore.innerText = "Show more";
+  }
+});
