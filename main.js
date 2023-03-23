a9480a4d8a14729ec3add29a9857b239e7f4e14e
DAVID <owolagbadavid@gmail.com>
2023-03-23 13:49:24 +0100

    edited form submit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let menu = document.querySelector("#menu-btn");
 let header = document.querySelector(".header");
 let more = document.getElementById("more");
 let showmore = document.getElementById("showmore");
+const form = document.getElementById("form-btn");
+const email = document.getElementById("client-email");
+const clientName = document.getElementById("client-name");
+const message = document.getElementById("client-message");
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   header.classList.toggle("active");
@@ -27,8 +31,6 @@ const progressBar = document.querySelectorAll(".progress div span");
 let i = 0;
 for (let span of progress) {
   progressBar[i++].style.width = `${span.textContent}`;
-
-  console.log(span.textContent);
 }
 showmore.addEventListener("click", () => {
   if (more.style.display == "none") {
@@ -39,3 +41,24 @@ showmore.addEventListener("click", () => {
     showmore.innerText = "Show more";
   }
 });
+
+form.onclick = function(e){
+  e.preventDefault();
+  const data = {email: email.value, name: clientName.value, message: message.value}
+  postData("https://formsubmit.co/ajax/8d5adb66a53b9ebdcb1b6e79488a69b3", data)
+  .then((res)=> alert("Your Message has been sent"))
+  .catch((err)=> alert("Error sending message, please try again"))
+}
+
+async function postData(url, data) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+  },
+  body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return "Message Sent" // parses JSON response into native JavaScript objects
+}
